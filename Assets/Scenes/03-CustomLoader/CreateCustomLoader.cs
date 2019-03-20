87cf8706fe43ca47c2b493ba868fae32e9066236
Qiu Cheng <qiucheng@sightp.com>
2019-03-20 15:00:29 +0800

    custom loader

---
+++ Assets/Scenes/03-CustomLoader/CreateCustomLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System.IO;
+public class CreateCustomLoader : MonoBehaviour {
+
+    private LuaEnv luaenv;
+    // Use this for initialization
+    void Start () {
+        luaenv = new LuaEnv();
+
+        luaenv.AddLoader(MyLoader);
+
+        luaenv.DoString("require 'test007'");
+    }
+
+    private byte[] MyLoader(ref string filePath)
+    {
+        print(filePath);
+
+        string absPath = Application.streamingAssetsPath + "/" + filePath + ".lua.txt";
+
+        return File.ReadAllBytes(absPath);
+
+        //return System.Text.Encoding.UTF8.GetBytes(str);
+    }
+
+    void OnDestroy () {
+        luaenv.Dispose();
+    }
+}
---
+++ Assets/Scenes/04-C#CallLua/CSharpCallLua.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class CSharpCallLua : MonoBehaviour {
+
+    private LuaEnv luaenv;
+
+    void Start () {
+        luaenv = new LuaEnv();
+
+        luaenv.DoString("require 'CSharpCallLua'");
+
+        //print(luaenv.Global.Get<int>("a"));
+        //print(luaenv.Global.Get<string>("str"));
+        //print(luaenv.Global.Get<bool>("isDie"));
+
+        Person p = luaenv.Global.Get<Person>("person"); //通过class来接受 值拷贝
+        print(p.Name + "-" + p.age);
+        p.eat(2, 5);
+
+        IPerson p2 = luaenv.Global.Get<IPerson>("person"); //通过interface来接受  引用拷贝
+        print(p2.Name + "-" + p2.age);
+        p2.eat(12,34);
+        p2.add(111, 222);
+        p2.Name = "John.q";
+        luaenv.DoString("print(person.Name)");
+    }
+
+    void OnDestroy () {
+        luaenv.Dispose();
+    }
+
+    class Person
+    {
+        public string Name;
+        public int age;
+        public void eat(int a, int b) {
+            print("a+b=" + (a + b));
+        }
+    }
+
+    [CSharpCallLua]
+    interface IPerson
+    {
+        string Name { get; set; }
+        int age { get; set; }
+        void eat(int a,int b);
+        void add(int a, int b);
+    }
+}
