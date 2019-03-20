0437dab96745c0e4ae9e25f320a4f4499b2bf9d5
Qiu Cheng <qiucheng@sightp.com>
2019-03-20 10:32:55 +0800

    update 03/20

---
+++ Assets/Scenes/01-LoadLua/HelloLua.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class HelloLua : MonoBehaviour {
+
+    private LuaEnv luaenv;
+
+    void Start () {
+        luaenv = new LuaEnv();
+
+        luaenv.DoString("print('Hello Lua!')");
+
+        luaenv.DoString("CS.UnityEngine.Debug.Log('Hello Qiucheng')");
+    }
+
+    private void OnDestroy()
+    {
+        luaenv.Dispose();
+    }
+}
---
+++ Assets/Scenes/02-LoadLuaByFile/HelloLua2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class HelloLua2 : MonoBehaviour {
+
+    private LuaEnv luaenv;
+    // Use this for initialization
+    void Start () {
+        luaenv = new LuaEnv();
+
+        luaenv.DoString("require 'byfile'");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (luaenv != null)
+        {
+            luaenv.Tick();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        luaenv.Dispose();
+    }
+}
